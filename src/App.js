57a5caaf43abae139e6083db53e398febe42086e
AmanRaSingh <developer.aman.sing>
2024-07-28 11:05:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import FirstNav from './Blog/Navigation/FirstNav';
+import SecondNav from './Blog/Navigation/SecondNav';
+import Hero from './Blog/Hero-Section/Hero';
+
+function App() {
+  return (
+   <>
+   <FirstNav/>
+   <SecondNav/>
+   <Hero/>
+   </>
+  );
+}
+
+export default App;
